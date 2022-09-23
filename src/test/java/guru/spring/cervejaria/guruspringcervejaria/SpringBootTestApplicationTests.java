232590008078aff8c68731678d9bc03c8ebd853f
Diego Oliveira <diegodimap@gmail.com>
2022-09-23 10:23:21 -0300

    initial commmit

---
+++ src/test/java/guru/spring/cervejaria/guruspringcervejaria/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.cervejaria.guruspringcervejaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
